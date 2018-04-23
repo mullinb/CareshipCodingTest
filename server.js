1d16892de2f8855d02f5b99ddd8a26a9f89446a4
Brian Mullin <mullin.mm@gmail.com>
2018-04-23 10:59:32 +0200

    add basic html UI for post request to server

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
+var path = require('path');
+
+app.use(express.json());
+app.use(express.urlencoded());
 
 const atm = require('./models/atm');
 
 app.get('/withdraw/:amount', (req, res) => {
   let result = atm(req.params.amount);
-  res.send(result);
+  res.json(result);
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/src/index.html'));
+})
+
+app.post('*', (req, res) => {
+  let result = atm(req.body.amount);
+  res.json(result);
 })
 
 app.listen(process.env.PORT || 8080, console.log("server is listening"));
